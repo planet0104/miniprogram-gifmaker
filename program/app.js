4cc883f4c7beef9a75c405417cb921a50f96f550
planet0104 <planet0104@gmail.com>
2018-11-16 18:09:35 +0800

    timeout

--- program/app.js
+++ program/app.js
@@ -1,14 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    var gifHelper = require("gif_helper.js");
-    var that = this;
-    gifHelper.then(function (helper) {
-      that.globalData.gifHelper = helper;
-    });
   },
   globalData: {
-    userInfo: null,
-    gifHelper: null,
   }
 })
